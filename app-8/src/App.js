261b06e7ac965de5b347a45ab2017e1f8566d137
DaTrueBrad <bottbrady@gmail.com>
2022-02-24 08:04:23 -0700

    Altered apps 8 and 9

--- app-8/src/App.js
+++ app-8/src/App.js
@@ -1,24 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [lukeSkywalker, setLukeSkywalker] = useState({})
+
+  useEffect(
+    axios
+      .get("https://swapi.co/api/people/1")
+      .then((res) => setLukeSkywalker(res.data))
+  , [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Name: {lukeSkywalker.name}</h1>
+      <h1>Birth Year: {lukeSkywalker.birth_year}</h1>
+      <h1>Height: {lukeSkywalker.height}</h1>
+      <h1>Eye Color: {lukeSkywalker.eye_color}</h1>
     </div>
   );
 }
